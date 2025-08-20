acde1e15d7195c93b3c596ea608c828d632a324f
elineviktoria <eline@tobiassen.no>
2025-08-20 14:10:49 +0200

    init: vite + react + tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main className="min-h-screen grid place-items-center">
+        <h1 className="text-3xl font-bold text-brand-500">Tailwind v4 funker</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
