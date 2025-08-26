04222357d78fd184659a451a9550b858529f055c
Helle Tomten Østli <152817250+helletomten@users.noreply.github.com>
2025-08-26 21:19:19 +0200

    Revert "collab karusell"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Collab from "../src/features/sections/Collab.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +10,6 @@ function App() {
     <>
       <main className="min-h-screen grid place-items-center">
         {/* <h1 className="text-3xl font-bold text-brand-500">Tailwind v4 funker</h1> */}
-        <Collab/>
       </main>
     </>
   )
