d53add7e5fd9df0e08376fbb748074b1a1aaa7bc
elineviktoria <eline@tobiassen.no>
2025-10-20 21:51:38 +0200

    implementert footer

---
+++ src/features/sections/Footer.jsx
@@ -0,0 +1,59 @@
+import "@/styles/footer.css";
+
+export default function Footer() {
+    return (
+        <section className="footer">
+            <div className="footer-container">
+                <div className="footer-brand">
+                    <a href="/" className="footer-logo">
+                        Therese Hommedal
+                    </a>
+                    <p className="footer-tagline">Makeupartist</p>
+                </div>
+
+                {/* Navigsjon */}
+                <nav className="footer-nav">
+                    <a href="/about">Om meg</a>
+                    <a href="/makeup">Galleri</a>
+                    <a href="/prices">Priser</a>
+                    <a href="/contact">Kontakt</a>
+                </nav>
+
+                {/* SoMe */}
+                <div className="footer-socials">
+                    <a
+                        href="https://www.instagram.com/theresehommedal/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Instagram
+                    </a>
+
+                    <a
+                        href="#"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Annen link
+                    </a>
+
+                    <a
+                        href="/contact"
+                        // target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Ta kontakt
+                    </a>
+                </div>
+
+                {/* Bunntekst */}
+                <div className="footer-bottom">
+                    <p>© {new Date().getFullYear()} Therese Hommedal</p>
+                    <p className="footer-credit">
+                        Utviklet av Ehlma Consulting
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
